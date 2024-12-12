e9b448dc7126f572b31e3b6718a5cd4b3ed8e459
venslyg <venslygunasekara2002@gmail.com>
2024-12-12 14:04:21 +0530

    supplier manager

--- AK Textile/AdminAdmin.cs
+++ AK Textile/AdminAdmin.cs
@@ -28,7 +28,7 @@ namespace AK_Textile
         {
             //Step 04
             //Load Employee Form in Main Panal
-            mainForm.LoadForm(new EmployeeManagerEmployee(mainForm));
+            mainForm.LoadForm(new EmpManagerDepartment(mainForm));
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
