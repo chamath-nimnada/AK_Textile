d5e6bd97b6fe6ec5f7d38a034cd46f0e07622520
venslyg <venslygunasekara2002@gmail.com>
2024-12-16 02:10:26 +0530

    admin changes

--- AK Textile/AdminAdmin.cs
+++ AK Textile/AdminAdmin.cs
@@ -7,12 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AK_Textile
 {
     public partial class AdminAdmin : Form
     {
         private MainForm mainForm; //Step 01
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-SDPNF2L\MSSQLSERVER01;
+                                                Initial Catalog=Textlies;
+                                                Integrated Security=True");
         public AdminAdmin(MainForm mainForm/*Step 02*/)
         {
             InitializeComponent();
@@ -38,7 +43,7 @@ namespace AK_Textile
 
         private void AdminAdmin_Load(object sender, EventArgs e)
         {
-
+            LoadAdmins();
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
@@ -67,6 +72,69 @@ namespace AK_Textile
         {
             AdminAdminRemove adremove = new AdminAdminRemove();
             adremove.ShowDialog();
+        }
+
+        public void LoadAdmins()
+        {
+                try
+                {
+                    con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Employee WHERE PositionID ='P003'", con);
+                    {
+                        // Execute the query and load the results into a DataTable
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        // Bind the DataTable to the DataGridView
+                        dataGridView1.DataSource = dt;
+
+                        // Adjust columns to fit the grid width
+                        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while loading data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                con.Close();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            LoadAdmins();
+        }
+
+        //method to search button to filter data according to the entered name or ID
+        private void SearchAdmin(string searchValue)
+        {
+            con.Open();
+            SqlCommand cmd1 = new SqlCommand("SELECT * FROM Employee WHERE EmpID LIKE @searchval OR EmpName LIKE @searchval", con);
+            cmd1.Parameters.AddWithValue("@searchval", "%" + searchValue + "%");
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd1);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //To get the  searched data
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                if (dt.Rows.Count == 0)
+                {
+                    MessageBox.Show("No Matching item Found", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+            }
+
         }
     }
 }
